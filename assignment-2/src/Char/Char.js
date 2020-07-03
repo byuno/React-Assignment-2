8583ad702c79d03f3f8f98428761781fba14e4be
Bobby Uno <robert.y.uno@gmail.com>
2020-07-03 12:10:32 -0700

    added inline styling

---
+++ assignment-2/src/Char/Char.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const char = () => {
+
+  const style ={
+    display: 'inline-lock',
+    padding: '16px',
+    margin: '16px',
+    border: '1px solid black',
+    textAlign: center
+
+  }
+  return(
+    <div style={style}>
+
+    </div>
+  )
+
+}
+export default char;
